945084052df7868758e3498223089f1906e5a79a
Lee Goddard <leegee@gmail.com>
2025-09-04 17:31:07 +0200

    Focus

--- src/components/WordSearch.tsx
+++ src/components/WordSearch.tsx
@@ -1,29 +1,37 @@
-import { createSignal } from "solid-js";
+import { createSignal, onMount } from "solid-js";
 
 type Props = {
     onSearch: (word: string) => void;
 };
 
 export default function WordSearch(props: Props) {
+    let inputRef: HTMLInputElement | undefined;
     const [value, setValue] = createSignal("");
 
     const submit = (e: Event) => {
         e.preventDefault();
         props.onSearch(value());
+        inputRef?.focus(); // Re-focus after submit
     };
+
+    onMount(() => inputRef?.focus());
 
     return (
         <form onSubmit={submit}>
             <nav class="no-space medium-width center padding">
                 <div class="field max border left-round">
                     <input
-                        type="text"
+                        name="search"
+                        type="search"
                         placeholder="Enter English word..."
                         value={value()}
                         onInput={e => setValue((e.target as HTMLInputElement).value)}
+                        ref={inputRef}
                     />
                 </div>
-                <button type="submit" class="large right-round">Search</button>
+                <button type="submit" class="large right-round">
+                    <i>search</i>
+                </button>
             </nav>
         </form>
     );
