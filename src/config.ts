ad5c396d0020d650c6a09d657c4b5cb555490483
Lee Goddard <leegee@gmail.com>
2025-09-08 11:08:11 +0200

    tidy

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// import { fileURLToPath } from "url";
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 // export const DB_FILE_PATH = path.resolve(__dirname, "../data/words.db");
+
 export const DB_FILE_PATH = path.join(process.cwd(), "data", "words.db");
 
