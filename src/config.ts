9addc03a5e2bc77dd1d0c0945504d2d3a82644b9
Lee Goddard <leegee@gmail.com>
2025-09-08 11:06:34 +0200

    DB path for Vercel

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,6 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export const DB_FILE_PATH = path.resolve(__dirname, "../data/words.db");
+// export const DB_FILE_PATH = path.resolve(__dirname, "../data/words.db");
+export const DB_FILE_PATH = path.join(process.cwd(), "data", "words.db");
+
